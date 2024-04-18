4e4ae93058decd02555335de412b08cf80b3909a
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-04-18 21:39:46 +0530

    changge

--- sps.js
+++ sps.js
@@ -18,6 +18,7 @@ const drawGame = () => {
     msg.style.backgroundColor = "#7c2e41";
 };
 
+/*Math.floor(Math.random()*10)---[from 0-9]*/
 
 const showWinner = (userwin,choiceid,comChoice) => {
     if (userwin) {
